c169f3a04f41c997d87080844b9f361144bdfb31
Terje Sylvarnes <108797958+terjesyl@users.noreply.github.com>
2024-10-28 14:12:50 +0100

    test skos

--- tests/test_shacl.py
+++ tests/test_shacl.py
@@ -8,20 +8,62 @@ REPO_PATH = pathlib.Path(__file__).parent.parent.resolve()
 
 SHAPE_GRAPH = "urn:digdir:shapes"
 
+
 def test_validate_example():
-    if "GITHUB_STEP_SUMMARY" in os.environ :
+    if "GITHUB_STEP_SUMMARY" in os.environ:
         f = open(os.environ["GITHUB_STEP_SUMMARY"], "a")
     else:
         f = None
     m = Mapping()
     m.read_triples(str(REPO_PATH / "examples" / "example-dataset.ttl"))
-    m.read_triples(str(REPO_PATH / "shacl" / "DCAT-AP-NO-shacl_shapes_2.00.ttl"), graph=SHAPE_GRAPH)
-    report = m.validate(shape_graph=SHAPE_GRAPH, include_conforms=False, include_shape_graph=True)
+    m.read_triples(
+        str(REPO_PATH / "shacl" / "DCAT-AP-NO-shacl_shapes_2.00.ttl"), graph=SHAPE_GRAPH
+    )
+    report = m.validate(
+        shape_graph=SHAPE_GRAPH, include_conforms=False, include_shape_graph=True
+    )
     results_df = report.results()
     if results_df is not None:
-        violations_df = results_df.filter(pl.col("result_severity").eq("<http://www.w3.org/ns/shacl#Violation>"))
+        violations_df = results_df.filter(
+            pl.col("result_severity").eq("<http://www.w3.org/ns/shacl#Violation>")
+        )
         if violations_df.height > 0:
-            print("\n__Example file validation failures:__ {}\n".format(violations_df.height), file=f)
+            print(
+                "\n__Example file validation failures:__ {}\n".format(
+                    violations_df.height
+                ),
+                file=f,
+            )
+            print(violations_df.to_pandas().to_markdown(index=False), file=f)
+            f.close()
+            assert violations_df.height == 0
+
+
+def test_validate_concept_example():
+    if "GITHUB_STEP_SUMMARY" in os.environ:
+        f = open(os.environ["GITHUB_STEP_SUMMARY"], "a")
+    else:
+        f = None
+    m = Mapping()
+    m.read_triples(str(REPO_PATH / "examples" / "out.ttl"))
+    m.read_triples(
+        str(REPO_PATH / "shacl" / "SKOS-AP-NO-shacl_shapes_2.00.ttl"), graph=SHAPE_GRAPH
+    )
+    report = m.validate(
+        shape_graph=SHAPE_GRAPH, include_conforms=False, include_shape_graph=True
+    )
+    results_df = report.results()
+    if results_df is not None:
+        violations_df = results_df.filter(
+            pl.col("result_severity").eq("<http://www.w3.org/ns/shacl#Violation>")
+        )
+        if violations_df.height > 0:
+            print(
+                "\n__Example file validation failures:__ {}\n".format(
+                    violations_df.height
+                ),
+                file=f,
+            )
             print(violations_df.to_pandas().to_markdown(index=False), file=f)
             f.close()
             assert violations_df.height == 0
